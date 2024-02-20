80957b8f111aec693ce55695ea08cdc6cd842a5d
Nicholas Krisna <nicholaskrisnapl@gmail.com>
2024-02-20 17:22:25 +0700

    adding few math function to helper

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,48 @@
 const puppeteer = require('puppeteer');
-const hbs = require('handlebars')
-const fs = require('fs-extra')
+const hbs = require('handlebars');
+const fs = require('fs-extra');
 
 export const createPdf = async (filePath: string, options = {}, data = {}) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    
-    hbs.registerHelper("ifCond", function (
-        v1: any, 
-        operator: any, 
-        v2: any, 
-        options:any
-      ) {
-      switch (operator) {
-        case "==":
-          return v1 == v2 ? options.fn(data) : options.inverse(data);
-        case "===":
-          return v1 === v2 ? options.fn(data) : options.inverse(data);
-        case "!=":
-          return v1 != v2 ? options.fn(data) : options.inverse(data);
-        case "!==":
-          return v1 !== v2 ? options.fn(data) : options.inverse(data);
-        case "<":
-          return v1 < v2 ? options.fn(data) : options.inverse(data);
-        case "<=":
-          return v1 <= v2 ? options.fn(data) : options.inverse(data);
-        case ">":
-          return v1 > v2 ? options.fn(data) : options.inverse(data);
-        case ">=":
-          return v1 >= v2 ? options.fn(data) : options.inverse(data);
-        case "&&":
-          return v1 && v2 ? options.fn(data) : options.inverse(data);
-        case "||":
-          return v1 || v2 ? options.fn(data) : options.inverse(data);
-        default:
-          return options.inverse(options);
+
+    hbs.registerHelper(
+      'ifCond',
+      function (v1: any, operator: any, v2: any, options: any) {
+        switch (operator) {
+          case '==':
+            return v1 == v2 ? options.fn(data) : options.inverse(data);
+          case '===':
+            return v1 === v2 ? options.fn(data) : options.inverse(data);
+          case '!=':
+            return v1 != v2 ? options.fn(data) : options.inverse(data);
+          case '!==':
+            return v1 !== v2 ? options.fn(data) : options.inverse(data);
+          case '<':
+            return v1 < v2 ? options.fn(data) : options.inverse(data);
+          case '<=':
+            return v1 <= v2 ? options.fn(data) : options.inverse(data);
+          case '>':
+            return v1 > v2 ? options.fn(data) : options.inverse(data);
+          case '>=':
+            return v1 >= v2 ? options.fn(data) : options.inverse(data);
+          case '&&':
+            return v1 && v2 ? options.fn(data) : options.inverse(data);
+          case '||':
+            return v1 || v2 ? options.fn(data) : options.inverse(data);
+          default:
+            return options.inverse(options);
+        }
       }
-    });
+    );
 
     hbs.registerHelper({
+      multiply: (v1: number, v2: number) => v1 * v2,
+      add: (v1: number, v2: number) => v1 + v2,
+      divide: (v1: number, v2: number) => v1 / v2,
+      substract: (v1: number, v2: number) => v1 - v2,
+      modulo: (v1: number, v2: number) => v1 % v2,
       eq: (v1: any, v2: any) => v1 === v2,
       ne: (v1: any, v2: any) => v1 !== v2,
       lt: (v1: number, v2: number) => v1 < v2,
@@ -47,13 +50,13 @@ export const createPdf = async (filePath: string, options = {}, data = {}) => {
       lte: (v1: number, v2: number) => v1 <= v2,
       gte: (v1: number, v2: number) => v1 >= v2,
       and() {
-          return Array.prototype.every.call(arguments, Boolean);
+        return Array.prototype.every.call(arguments, Boolean);
       },
       or() {
-          return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
-      }
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+      },
     });
-    
+
     const html = await fs.readFile(filePath, 'utf8');
     const content = hbs.compile(html)(data);
     await page.setContent(content);
@@ -73,7 +76,7 @@ export const createPdf = async (filePath: string, options = {}, data = {}) => {
     await browser.close();
     // process.exit();
     return buffer;
-    } catch (e) {
+  } catch (e) {
     console.log(e);
-    }
+  }
 };
